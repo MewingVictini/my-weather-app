48778f2910efcf0bd3bcf84b07b1774c8c9f0aae
MewingVictini <77203427+MewingVictini@users.noreply.github.com>
2021-03-07 12:21:47 +0100

    Updated the curren time and day

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+function showCurrentTime(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+return `${day}, ${hours}:${minutes}`;
+}
+
+
+
 function displayTemperature(response) {
 let displayedTemperature = document.querySelector("#displayed-temperature");
 displayedTemperature.innerHTML = Math.round(response.data.main.temp);
@@ -11,6 +28,8 @@ let windSpeed = document.querySelector("#wind-speed");
 windSpeed.innerHTML = Math.round(response.data.wind.speed);
 let pressureElement = document.querySelector("#pressure");
 pressureElement.innerHTML = response.data.main.pressure;
+let currentTime = document.querySelector("#current-time");
+currentTime.innerHTML = showCurrentTime(response.data.dt * 1000);
 }
 
 
